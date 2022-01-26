68ba1ba47721e8ef6af782b59fcc82a3ca767d75
Caren Chang <carenc@google.com>
2022-01-26 13:13:25 -0800

    Resource Card: Add bookmark icon
    Change-Id: I61ae5ca4ba542415165265dfdf0aea4ee0a63f44

--- app/src/main/java/com/google/samples/apps/nowinandroid/ui/ResourceCard.kt
+++ app/src/main/java/com/google/samples/apps/nowinandroid/ui/ResourceCard.kt
@@ -17,9 +17,19 @@
 package com.google.samples.apps.nowinandroid.ui
 
 import android.content.res.Configuration
-import androidx.compose.material.Surface
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Bookmark
+import androidx.compose.material.icons.filled.BookmarkBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconToggleButton
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.onClick
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.samples.apps.nowinandroid.R
 import com.google.samples.apps.nowinandroid.data.news.NewsResource
 import com.google.samples.apps.nowinandroid.ui.theme.NiaTheme
 
@@ -27,6 +37,31 @@ import com.google.samples.apps.nowinandroid.ui.theme.NiaTheme
  * [com.google.samples.apps.nowinandroid.data.news.NewsResource] card used on the following screens:
  * For You, Episodes, Saved
  */
+
+@Composable
+fun BookmarkButton(
+    isBookmarked: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val clickActionLabel = stringResource(
+        if (isBookmarked) R.string.unbookmark else R.string.bookmark
+    )
+    IconToggleButton(
+        checked = isBookmarked,
+        onCheckedChange = { onClick() },
+        modifier = modifier.semantics {
+            // Use custom label for accessibility services to communicate button's action to user.
+            // Pass null for action to only override the label and not the actual action.
+            this.onClick(label = clickActionLabel, action = null)
+        }
+    ) {
+        Icon(
+            imageVector = if (isBookmarked) Icons.Filled.Bookmark else Icons.Filled.BookmarkBorder,
+            contentDescription = null // handled by click label of parent
+        )
+    }
+}
 
 @Composable
 fun ResourceAuthors(
@@ -80,6 +115,26 @@ fun ResourceTopics(
 @Composable
 fun ResourceCardExpanded() {
     TODO()
+}
+
+@Preview("Bookmark Button")
+@Composable
+fun BookmarkButtonPreview() {
+    NiaTheme {
+        Surface {
+            BookmarkButton(isBookmarked = false, onClick = { })
+        }
+    }
+}
+
+@Preview("Bookmark Button Bookmarked")
+@Composable
+fun BookmarkButtonBookmarkedPreview() {
+    NiaTheme {
+        Surface {
+            BookmarkButton(isBookmarked = true, onClick = { })
+        }
+    }
 }
 
 @Preview("Expanded resource card")
