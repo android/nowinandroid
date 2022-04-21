707c9d04e785d382100de8fa2860fe82d8327f7c
Miłosz Moczkowski <mmoczkowski@google.com>
2022-04-21 16:38:32 +0000

    Revert "Add TopAppBar to the "For You screen""
    This reverts commit 3e664f1b724a620ad606cda0d4758240d80073ab.
    
    Reason for revert: This change doesn't pass one of the tests
    
    Change-Id: I9ffacbe4eaf1080323a7c9073dfe426a8f43345c

--- feature-following/src/main/java/com/google/samples/apps/nowinandroid/feature/following/FollowingScreen.kt
+++ feature-following/src/main/java/com/google/samples/apps/nowinandroid/feature/following/FollowingScreen.kt
@@ -18,9 +18,6 @@ package com.google.samples.apps.nowinandroid.feature.following
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -31,9 +28,9 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.google.samples.apps.nowinandroid.core.ui.NiaLoadingIndicator
+import com.google.samples.apps.nowinandroid.core.ui.NiaToolbar
 import com.google.samples.apps.nowinandroid.core.ui.component.NiaTab
 import com.google.samples.apps.nowinandroid.core.ui.component.NiaTabRow
-import com.google.samples.apps.nowinandroid.core.ui.component.NiaTopAppBar
 
 @Composable
 fun InterestsRoute(
@@ -72,17 +69,7 @@ fun FollowingScreen(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        NiaTopAppBar(
-            titleRes = R.string.interests,
-            navigationIcon = Icons.Filled.Search,
-            navigationIconContentDescription = stringResource(
-                id = R.string.top_app_bar_navigation_button_content_desc
-            ),
-            actionIcon = Icons.Filled.MoreVert,
-            actionIconContentDescription = stringResource(
-                id = R.string.top_app_bar_navigation_button_content_desc
-            )
-        )
+        NiaToolbar(titleRes = R.string.interests)
         when (uiState) {
             FollowingUiState.Loading ->
                 NiaLoadingIndicator(
