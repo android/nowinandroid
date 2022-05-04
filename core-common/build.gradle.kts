59c5979c0a27e7926e323a80d75f1ec7b6968e76
Adetunji Dahunsi <dahunsi@google.com>
2022-05-04 12:40:13 -0400

    Catch exceptions in the flow.asResult method
    Change-Id: I0289e535b8811be95b58b2ea4abe515ab4191cfc

--- core-common/build.gradle.kts
+++ core-common/build.gradle.kts
@@ -24,4 +24,6 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
+
+    testImplementation(project(":core-testing"))
 }
--- core-common/src/main/java/com/google/samples/apps/nowinandroid/core/result/Result.kt
+++ core-common/src/main/java/com/google/samples/apps/nowinandroid/core/result/Result.kt
@@ -17,6 +17,7 @@
 package com.google.samples.apps.nowinandroid.core.result
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 
@@ -32,4 +33,5 @@ fun <T> Flow<T>.asResult(): Flow<Result<T>> {
             Result.Success(it)
         }
         .onStart { emit(Result.Loading) }
+        .catch { emit(Result.Error(it)) }
 }
