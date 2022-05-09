7a5af7510559fa506c2c9b478c96318a70305a4f
Alex Vanyo <vanyo@google.com>
2022-05-09 14:39:20 -0700

    Update for you top app bar
    Change-Id: I95c8082198e1d1068e9ef674b005d7999e1b4542

--- core-ui/src/main/java/com/google/samples/apps/nowinandroid/core/ui/component/TopAppBar.kt
+++ core-ui/src/main/java/com/google/samples/apps/nowinandroid/core/ui/component/TopAppBar.kt
@@ -25,6 +25,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -40,6 +42,7 @@ fun NiaTopAppBar(
     actionIcon: ImageVector,
     actionIconContentDescription: String?,
     modifier: Modifier = Modifier,
+    colors: TopAppBarColors = TopAppBarDefaults.centerAlignedTopAppBarColors(),
     onNavigationClick: () -> Unit = {},
     onActionClick: () -> Unit = {}
 ) {
@@ -63,6 +66,7 @@ fun NiaTopAppBar(
                 )
             }
         },
+        colors = colors,
         modifier = modifier
     )
 }
