7cf869392384c02d14c0735e49eb1852df995e2c
Simona Stojanovic <anomis@google.com>
2022-05-13 15:46:20 +0100

    Add common build convention for feature modules
    Change-Id: I24d54343de2835fff4f7a8dfc598b9a1864ffec7

--- build-logic/convention/src/main/kotlin/nowinandroid.android.library.gradle.kts
+++ build-logic/convention/src/main/kotlin/nowinandroid.android.library.gradle.kts
@@ -28,3 +28,16 @@ android {
         targetSdk = 32
     }
 }
+
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+dependencies {
+    // androidx.test is forcing JUnit, 4.12. This forces it to use 4.13
+    configurations.configureEach {
+        resolutionStrategy {
+            force(libs.findLibrary("junit4").get())
+            // Temporary workaround for https://issuetracker.google.com/174733673
+            force("org.objenesis:objenesis:2.6")
+        }
+    }
+}
