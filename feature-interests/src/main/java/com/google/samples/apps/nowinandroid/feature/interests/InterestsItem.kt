a72bc0a16a79a31dab2554baf6656da4d6fc9364
Simon Marquis <contact@simon-marquis.fr>
2022-05-19 21:01:31 +0200

    Fix format violation
    Command: ./gradlew :feature-interests:spotlessApply

--- feature-interests/src/main/java/com/google/samples/apps/nowinandroid/feature/interests/InterestsItem.kt
+++ feature-interests/src/main/java/com/google/samples/apps/nowinandroid/feature/interests/InterestsItem.kt
@@ -33,7 +33,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
