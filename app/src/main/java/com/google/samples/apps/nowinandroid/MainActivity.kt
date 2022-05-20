4f5797565770d4b2b7241e4f87fb340e7bebf8ff
Ben Weiss <benweiss@google.com>
2022-05-20 16:24:32 +0100

    Add jankStats and rudamentary jank logging

--- app/src/main/java/com/google/samples/apps/nowinandroid/MainActivity.kt
+++ app/src/main/java/com/google/samples/apps/nowinandroid/MainActivity.kt
@@ -17,17 +17,33 @@
 package com.google.samples.apps.nowinandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.remember
 import androidx.core.view.WindowCompat
+import androidx.metrics.performance.JankStats
+import androidx.metrics.performance.PerformanceMetricsState
 import com.google.samples.apps.nowinandroid.ui.NiaApp
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asExecutor
 
 @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var jankStats: JankStats
+
+    private val jankFrameListener = JankStats.OnFrameListener { frameData ->
+        // Make sure to only log janky frames.
+        if (frameData.isJank) {
+            Log.v("NiA Jank", frameData.toString())
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -35,6 +51,24 @@ class MainActivity : ComponentActivity() {
         // including IME animations
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        setContent { NiaApp(calculateWindowSizeClass(this)) }
+        setContent {
+            NiaApp(calculateWindowSizeClass(this))
+        }
+
+        jankStats = JankStats.createAndTrack(
+            window,
+            Dispatchers.Default.asExecutor(),
+            jankFrameListener
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        jankStats.isTrackingEnabled = true
+    }
+
+    override fun onPause() {
+        super.onPause()
+        jankStats.isTrackingEnabled = false
     }
 }
