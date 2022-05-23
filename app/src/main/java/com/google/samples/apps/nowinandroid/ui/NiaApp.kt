155b3c3f428ad582883d8a63d2e8216d28cc7a2f
Saeed Noshadi <SaeedNoshadi89@gmail.com>
2022-05-23 15:23:47 +0430

    rebase

--- app/src/main/java/com/google/samples/apps/nowinandroid/ui/NiaApp.kt
+++ app/src/main/java/com/google/samples/apps/nowinandroid/ui/NiaApp.kt
@@ -39,13 +39,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
