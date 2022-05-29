7c761c5595f674890bd3e1e562cc5b600281d08c
Satyan Jacquens <satyan@nabla.com>
2022-05-29 20:00:39 +0200

    Keep targetSdk 31

--- build-logic/convention/src/main/kotlin/AndroidTestConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidTestConventionPlugin.kt
@@ -30,7 +30,7 @@ class AndroidTestConventionPlugin : Plugin<Project> {
 
             extensions.configure<TestExtension> {
                 configureKotlinAndroid(this)
-                defaultConfig.targetSdk = 32
+                defaultConfig.targetSdk = 31
             }
         }
     }
