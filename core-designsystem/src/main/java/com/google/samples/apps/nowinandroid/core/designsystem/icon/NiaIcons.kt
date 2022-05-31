1f984bef262d9f6ab5d6adaba72cc6f3c2f62971
Nick Rout <nickrout@google.com>
2022-05-31 08:29:07 +0200

    Improve design system usage and add lint checks
    Change-Id: I05f9400a9410e31bdf1534e458d560fe793b809f

---
+++ core-designsystem/src/main/java/com/google/samples/apps/nowinandroid/core/designsystem/icon/NiaIcons.kt
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid.core.designsystem.icon
+
+import androidx.annotation.DrawableRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.ArrowDropDown
+import androidx.compose.material.icons.rounded.ArrowDropUp
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.ExpandLess
+import androidx.compose.material.icons.rounded.Fullscreen
+import androidx.compose.material.icons.rounded.Grid3x3
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material.icons.rounded.ShortText
+import androidx.compose.material.icons.rounded.Tag
+import androidx.compose.material.icons.rounded.ViewDay
+import androidx.compose.material.icons.rounded.VolumeOff
+import androidx.compose.material.icons.rounded.VolumeUp
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.google.samples.apps.nowinandroid.core.designsystem.R
+
+/**
+ * Now in Android icons. Material icons are [ImageVector]s, custom icons are drawable resource IDs.
+ */
+object NiaIcons {
+    val AccountCircle = Icons.Outlined.AccountCircle
+    val Add = Icons.Rounded.Add
+    val ArrowBack = Icons.Rounded.ArrowBack
+    val ArrowDropDown = Icons.Rounded.ArrowDropDown
+    val ArrowDropUp = Icons.Rounded.ArrowDropUp
+    val Bookmark = R.drawable.ic_bookmark
+    val BookmarkBorder = R.drawable.ic_bookmark_border
+    val Bookmarks = R.drawable.ic_bookmarks
+    val BookmarksBorder = R.drawable.ic_bookmarks_border
+    val Check = Icons.Rounded.Check
+    val Close = Icons.Rounded.Close
+    val ExpandLess = Icons.Rounded.ExpandLess
+    val Fullscreen = Icons.Rounded.Fullscreen
+    val Grid3x3 = Icons.Rounded.Grid3x3
+    val MenuBook = R.drawable.ic_menu_book
+    val MenuBookBorder = R.drawable.ic_menu_book_border
+    val MoreVert = Icons.Default.MoreVert
+    val Person = Icons.Rounded.Person
+    val PlayArrow = Icons.Rounded.PlayArrow
+    val Search = Icons.Rounded.Search
+    val ShortText = Icons.Rounded.ShortText
+    val Tag = Icons.Rounded.Tag
+    val Upcoming = R.drawable.ic_upcoming
+    val UpcomingBorder = R.drawable.ic_upcoming_border
+    val ViewDay = Icons.Rounded.ViewDay
+    val VolumeOff = Icons.Rounded.VolumeOff
+    val VolumeUp = Icons.Rounded.VolumeUp
+}
+
+/**
+ * A sealed class to make dealing with [ImageVector] and [DrawableRes] icons easier.
+ */
+sealed class Icon {
+    data class ImageVectorIcon(val imageVector: ImageVector) : Icon()
+    data class DrawableResourceIcon(@DrawableRes val id: Int) : Icon()
+}
