d9ddde10ea7dc38d4cef7914d5e1508ca6461407
Tomáš Mlynarič <mlykotom@google.com>
2022-06-27 18:20:33 +0200

    Improve generator to scroll feed + interests
    Change-Id: I9e8728813a8107f7b15496bef719add27e7b8740

--- benchmark/src/main/java/com/google/samples/apps/nowinandroid/baselineprofile/BaselineProfileGenerator.kt
+++ benchmark/src/main/java/com/google/samples/apps/nowinandroid/baselineprofile/BaselineProfileGenerator.kt
@@ -19,8 +19,12 @@ package com.google.samples.apps.nowinandroid.baselineprofile
 import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import androidx.test.uiautomator.By
-import androidx.test.uiautomator.Direction
 import com.google.samples.apps.nowinandroid.PACKAGE_NAME
+import com.google.samples.apps.nowinandroid.foryou.forYouScrollFeedDownUp
+import com.google.samples.apps.nowinandroid.foryou.forYouSelectAuthors
+import com.google.samples.apps.nowinandroid.foryou.forYouWaitForContent
+import com.google.samples.apps.nowinandroid.interests.interestsScrollPeopleDownUp
+import com.google.samples.apps.nowinandroid.interests.interestsScrollTopicsDownUp
 import org.junit.Rule
 import org.junit.Test
 
@@ -32,25 +36,31 @@ class BaselineProfileGenerator {
     @get:Rule val baselineProfileRule = BaselineProfileRule()
 
     @Test
-    fun startup() =
+    fun generate() =
         baselineProfileRule.collectBaselineProfile(PACKAGE_NAME) {
-            pressHome()
             // This block defines the app's critical user journey. Here we are interested in
             // optimizing for app startup. But you can also navigate and scroll
             // through your most important UI.
+
+            pressHome()
             startActivityAndWait()
+
+            // Scroll the feed critical user journey
+            forYouWaitForContent()
+            forYouSelectAuthors()
+            forYouScrollFeedDownUp()
+
+            // Navigate to interests screen            
+            device.findObject(By.text("Interests")).click()
             device.waitForIdle()
 
-            device.run {
-                findObject(By.text("Interests"))
-                    .click()
-                waitForIdle()
-                findObject(By.text("Accessibility")).scroll(Direction.DOWN, 2000f)
-                waitForIdle()
-                findObject(By.text("People")).click()
-                waitForIdle()
-                findObject(By.textStartsWith("Android")).scroll(Direction.DOWN, 2000f)
-                waitForIdle()
-            }
+            interestsScrollTopicsDownUp()
+
+            // Navigate to people tab
+            device.findObject(By.text("People")).click()
+            device.waitForIdle()
+
+            interestsScrollPeopleDownUp()
         }
 }
+
