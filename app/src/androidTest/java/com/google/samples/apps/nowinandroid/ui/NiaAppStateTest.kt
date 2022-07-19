af7e2517f23d780900b83b5c6bcec2903f4074c7
Manuel Vivo <mvivo@google.com>
2022-07-19 14:07:59 +0200

    Add state holder for NiaApp composable

---
+++ app/src/androidTest/java/com/google/samples/apps/nowinandroid/ui/NiaAppStateTest.kt
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid.ui
+
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.compose.composable
+import androidx.navigation.createGraph
+import androidx.navigation.testing.TestNavHostController
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Rule
+import org.junit.Test
+
+/**
+ * Tests [NiaAppState].
+ *
+ * Note: This could become an unit test if Robolectric is added to the project and the Context
+ * is faked.
+ */
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+class NiaAppStateTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    private lateinit var state: NiaAppState
+
+    @Test
+    fun niaAppState_currentDestination() {
+        var currentDestination: String? = null
+
+        composeTestRule.setContent {
+            val navController = rememberTestNavController()
+            state = remember(navController) {
+                NiaAppState(
+                    windowSizeClass = getCompactWindowClass(),
+                    navController = navController
+                )
+            }
+
+            // Update currentDestination whenever it changes
+            currentDestination = state.currentDestination?.route
+
+            // Navigate to destination b once
+            LaunchedEffect(Unit) {
+                navController.setCurrentDestination("b")
+            }
+        }
+
+        assertEquals("b", currentDestination)
+    }
+
+    @Test
+    fun niaAppState_destinations() {
+        composeTestRule.setContent {
+            state = rememberNiaAppState(getCompactWindowClass())
+        }
+
+        assertEquals(2, state.topLevelDestinations.size)
+        assertTrue(state.topLevelDestinations[0].destination.contains("for_you"))
+        assertTrue(state.topLevelDestinations[1].destination.contains("interests"))
+    }
+
+    @Test
+    fun niaAppState_showBottomBar_compact() {
+        composeTestRule.setContent {
+            state = NiaAppState(
+                windowSizeClass = getCompactWindowClass(),
+                navController = NavHostController(LocalContext.current)
+            )
+        }
+
+        assertTrue(state.shouldShowBottomBar)
+        assertFalse(state.shouldShowNavRail)
+    }
+
+    @Test
+    fun niaAppState_showNavRail_medium() {
+        composeTestRule.setContent {
+            state = NiaAppState(
+                windowSizeClass = WindowSizeClass.calculateFromSize(DpSize(800.dp, 800.dp)),
+                navController = NavHostController(LocalContext.current)
+            )
+        }
+
+        assertTrue(state.shouldShowNavRail)
+        assertFalse(state.shouldShowBottomBar)
+    }
+
+    @Test
+    fun niaAppState_showNavRail_large() {
+        composeTestRule.setContent {
+            state = NiaAppState(
+                windowSizeClass = WindowSizeClass.calculateFromSize(DpSize(900.dp, 1200.dp)),
+                navController = NavHostController(LocalContext.current)
+            )
+        }
+
+        assertTrue(state.shouldShowNavRail)
+        assertFalse(state.shouldShowBottomBar)
+    }
+
+    private fun getCompactWindowClass() = WindowSizeClass.calculateFromSize(DpSize(500.dp, 300.dp))
+}
+
+@Composable
+private fun rememberTestNavController(): TestNavHostController {
+    val context = LocalContext.current
+    val navController = remember {
+        TestNavHostController(context).apply {
+            navigatorProvider.addNavigator(ComposeNavigator())
+            graph = createGraph(startDestination = "a") {
+                composable("a") { }
+                composable("b") { }
+                composable("c") { }
+            }
+        }
+    }
+    return navController
+}
