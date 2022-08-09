aa5d254435ee6f89c4b54e643c4812b3f9e17d21
Wojtek Kaliciński <wkal@google.com>
2022-08-09 15:41:35 +0200

    Use [plugins] block in version catalog
    Change-Id: Ib2aa30405dfe8e5f5017f2f1b45e972d8e19ddca

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,11 +23,13 @@ buildscript {
         maven { url = uri("../nowinandroid-prebuilts/m2repository") }
     }
 
-    dependencies {
-        classpath(libs.android.gradlePlugin)
-        classpath(libs.kotlin.gradlePlugin)
-        classpath(libs.kotlin.serializationPlugin)
-        classpath(libs.hilt.gradlePlugin)
-        classpath(libs.secrets.gradlePlugin)
-    }
+}
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.secrets) apply false
+    alias(libs.plugins.spotless) apply false
 }
