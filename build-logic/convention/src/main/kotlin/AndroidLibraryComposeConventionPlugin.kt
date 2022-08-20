1d34c6a8f7a9139261d3f9a815be02df450271d7
stylianosgakis <stylianos.gakis98@gmail.com>
2022-08-20 16:54:48 +0200

    Remove redundant configuration inside AndroidLibraryComposeConventionPlugin

--- build-logic/convention/src/main/kotlin/AndroidLibraryComposeConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryComposeConventionPlugin.kt
@@ -23,7 +23,6 @@ import org.gradle.kotlin.dsl.getByType
 class AndroidLibraryComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
-            pluginManager.apply("com.android.library")
             val extension = extensions.getByType<LibraryExtension>()
             configureAndroidCompose(extension)
         }
