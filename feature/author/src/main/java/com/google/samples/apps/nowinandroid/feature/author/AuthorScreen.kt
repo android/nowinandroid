9ae03c35907170388a7df3197009ae321a3fb8dc
Takeshi Hagikura <thagikura@google.com>
2022-09-20 17:30:49 +0900

    Fix the lint warnings

--- feature/author/src/main/java/com/google/samples/apps/nowinandroid/feature/author/AuthorScreen.kt
+++ feature/author/src/main/java/com/google/samples/apps/nowinandroid/feature/author/AuthorScreen.kt
@@ -44,7 +44,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
