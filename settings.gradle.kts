7afcef249e408e395b2eb4adf6c52fb07d0b5e03
Inaki Villar <inaki.seri@gmail.com>
2022-10-17 11:25:21 -0700

    setting base branch
    Change-Id: Iba8c714e93bc76344c80e524fab841a19f554aff

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,24 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.8.1"
+    id("com.gradle.enterprise") version "3.11.1"
+
+}
+
+gradleEnterprise {
+    server = "http://ge.solutions-team.gradle.com"
+    allowUntrustedServer = true
+    buildScan {
+        publishAlways()
+        capture {
+            isTaskInputFiles = true
+        }
+        isUploadInBackground = System.getenv("CI") == null
+    }
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
