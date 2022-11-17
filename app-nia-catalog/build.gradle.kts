c3d523afc666a52a4716a78c7c378b519f2f218c
mlykotom <mlykotom@google.com>
2022-11-17 13:15:39 +0100

    Add release config to catalog to prevent AS complaining

--- app-nia-catalog/build.gradle.kts
+++ app-nia-catalog/build.gradle.kts
@@ -21,6 +21,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.google.samples.apps.niacatalog"
+        versionCode = 1
+        versionName = "0.0.1" // X.Y.Z; X = Major, Y = minor, Z = Patch level
 
         // The UI catalog does not depend on content from the app, however, it depends on modules
         // which do, so we must specify a default value for the contentType dimension.
@@ -33,6 +35,15 @@ android {
         }
     }
     namespace = "com.google.samples.apps.niacatalog"
+
+    buildTypes {
+        val release by getting {
+            // To publish on the Play store a private signing key is required, but to allow anyone
+            // who clones the code to sign and run the release variant, use the debug signing key.
+            // TODO: Abstract the signing configuration to a separate file to avoid hardcoding this.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
 }
 
 dependencies {
