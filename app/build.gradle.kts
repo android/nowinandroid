62e4325c52c324e74b2c7739670287acea6cbce2
mlykotom <mlykotom@google.com>
2022-11-23 18:25:44 +0100

    Fix code style

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,5 @@
-import com.google.samples.apps.nowinandroid.NiABuildType
-
 /*
- * Copyright 2021 The Android Open Source Project
+ * Copyright 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +13,7 @@ import com.google.samples.apps.nowinandroid.NiABuildType
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import com.google.samples.apps.nowinandroid.NiABuildType
 
 plugins {
     id("nowinandroid.android.application")
