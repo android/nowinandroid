b13096140c293a220c9a398cf3c583e09209dd1e
mlykotom <mlykotom@google.com>
2022-12-06 11:21:24 +0100

    Add niaTopAppBar testTag
    Change-Id: I02a207675e106137fe005140965c96f1b50f98ce

--- core/designsystem/src/main/java/com/google/samples/apps/nowinandroid/core/designsystem/component/TopAppBar.kt
+++ core/designsystem/src/main/java/com/google/samples/apps/nowinandroid/core/designsystem/component/TopAppBar.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.nowinandroid.core.designsystem.icon.NiaIcons
@@ -66,7 +67,7 @@ fun NiaTopAppBar(
             }
         },
         colors = colors,
-        modifier = modifier
+        modifier = modifier.testTag("niaTopAppBar"),
     )
 }
 
@@ -95,7 +96,7 @@ fun NiaTopAppBar(
             }
         },
         colors = colors,
-        modifier = modifier,
+        modifier = modifier.testTag("niaTopAppBar"),
     )
 }
 
