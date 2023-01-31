443be42ffe1cf50924a33302dcba839015ea7d80
vishesh <visheshtiwari211@gmail.com>
2023-01-31 23:53:48 +0530

    bug: fixed bug #534 of multiple clicks on the interests screen

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ buildscript {
 }
 
 plugins {
+    java
+    application
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
