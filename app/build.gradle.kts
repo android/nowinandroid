590f695c14ecdf292f1f660fcf4f440ee6ede2b2
Don Turner <donturner@google.com>
2023-02-09 18:11:07 +0000

    Bump versionCode to 5
    Change-Id: I4d6dc6b8d1a903172b7f11ab6166f6c631b1319b

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.google.samples.apps.nowinandroid"
-        versionCode = 4
-        versionName = "0.0.4" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+        versionCode = 5
+        versionName = "0.0.5" // X.Y.Z; X = Major, Y = minor, Z = Patch level
 
         // Custom test runner to set up Hilt dependency graph
         testInstrumentationRunner = "com.google.samples.apps.nowinandroid.core.testing.NiaTestRunner"
