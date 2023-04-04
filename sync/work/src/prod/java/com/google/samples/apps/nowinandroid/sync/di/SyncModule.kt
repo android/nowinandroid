d2d42e109521fb2b0cf1fe36b0415dbf747c737f
Adetunji Dahunsi <dahunsi@google.com>
2023-04-04 09:24:32 -0400

    Wire up backend requested sync
    Change-Id: I1d4485b589c7e94527a2a02f371cd3f030231622

---
+++ sync/work/src/prod/java/com/google/samples/apps/nowinandroid/sync/di/SyncModule.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid.sync.di
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.ktx.messaging
+import com.google.samples.apps.nowinandroid.core.data.util.SyncManager
+import com.google.samples.apps.nowinandroid.sync.status.FirebaseSyncSubscriber
+import com.google.samples.apps.nowinandroid.sync.status.SyncSubscriber
+import com.google.samples.apps.nowinandroid.sync.status.WorkManagerSyncManager
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+interface SyncModule {
+    @Binds
+    fun bindsSyncStatusMonitor(
+        syncStatusMonitor: WorkManagerSyncManager,
+    ): SyncManager
+
+    @Binds
+    fun bindsSyncSubscriber(
+        syncSubscriber: FirebaseSyncSubscriber,
+    ): SyncSubscriber
+
+    companion object {
+        @Provides
+        @Singleton
+        fun provideFirebaseMessaging(): FirebaseMessaging = Firebase.messaging
+    }
+}
