119550fe389f9bf2437e0fde4e9cd066e878c0ca
Simon Marquis <contact@simon-marquis.fr>
2023-04-16 22:50:09 +0200

    Remove `DSL_SCOPE_VIOLATION` suppression as it is now fixed in Gradle 8.1

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-// TODO: Remove once https://youtrack.jetbrains.com/issue/KTIJ-19369 is fixed
-@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("nowinandroid.android.library")
     id("nowinandroid.android.library.jacoco")
