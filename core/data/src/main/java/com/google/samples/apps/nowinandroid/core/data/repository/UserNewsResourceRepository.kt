f2680a8973a7d8ef02eaff268a40cad808e7c198
James Rose <rosej@google.com>
2023-04-17 13:52:58 -0700

    Add missing method doc

--- core/data/src/main/java/com/google/samples/apps/nowinandroid/core/data/repository/UserNewsResourceRepository.kt
+++ core/data/src/main/java/com/google/samples/apps/nowinandroid/core/data/repository/UserNewsResourceRepository.kt
@@ -39,7 +39,7 @@ interface UserNewsResourceRepository {
     fun observeAllForFollowedTopics(): Flow<List<UserNewsResource>>
 
     /**
-     *
+     * Returns the user's bookmarked news resources as a stream.
      */
     fun observeAllBookmarked(): Flow<List<UserNewsResource>>
 }
