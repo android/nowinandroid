Hackermrj/mrj

Implement recent searches
- Add Repository and Dao for recent searches
- Add recent searches body in the Search UI
- Add relevant tests

core/domain/src/main/java/com/google/samples/apps/nowinandroid/core/domain/GetRecentSearchQueriesUseCase.kt
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid.core.domain
+
+import com.google.samples.apps.nowinandroid.core.data.repository.RecentSearchQuery
+import com.google.samples.apps.nowinandroid.core.data.repository.RecentSearchRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+/**
+ * A use case which returns the recent search queries.
+ */
+class GetRecentSearchQueriesUseCase @Inject constructor(
+    private val recentSearchRepository: RecentSearchRepository,
+) {
+    operator fun invoke(limit: Int = 10): Flow<List<RecentSearchQuery>> =
+        recentSearchRepository.getRecentSearchQueries(limit)
+}

core/testing/src/main/java/com/google/samples/apps/nowinandroid/core/testing/repository/TestRecentSearchRepository.kt
@@ -1,0 +1,41 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid.core.testing.repository
+
+import com.google.samples.apps.nowinandroid.core.data.repository.RecentSearchQuery
+import com.google.samples.apps.nowinandroid.core.data.repository.RecentSearchRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class TestRecentSearchRepository : RecentSearchRepository {
+
+    private val cachedRecentSearches: MutableList<RecentSearchQuery> = mutableListOf()
+
+    override fun getRecentSearchQueries(limit: Int): Flow<List<RecentSearchQuery>> {
+        return flow {
+            emit(cachedRecentSearches.sortedByDescending { it.queriedDate }.take(limit))
+        }
+    }
+
+    override suspend fun insertOrReplaceRecentSearch(searchQuery: String) {
+        cachedRecentSearches.add(RecentSearchQuery(searchQuery))
+    }
+
+    override suspend fun clearRecentSearches() {
+        cachedRecentSearches.clear()
+    }
+}

feature/search/src/androidTest/java/com/google/samples/apps/nowinandroid/feature/search/SearchScreenTest.kt
@@ -23,8 +23,9 @@ import androidx.compose.ui.test.assertIsFocused
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onParent
+import com.google.samples.apps.nowinandroid.core.data.repository.RecentSearchQuery
 import com.google.samples.apps.nowinandroid.core.domain.model.UserNewsResource
 import com.google.samples.apps.nowinandroid.core.model.data.DarkThemeConfig.DARK
 import com.google.samples.apps.nowinandroid.core.model.data.ThemeBrand.ANDROID
@@ -47,6 +48,7 @@ class SearchScreenTest {
     private lateinit var clearSearchContentDesc: String
     private lateinit var followButtonContentDesc: String
     private lateinit var unfollowButtonContentDesc: String
+    private lateinit var clearRecentSearchesContentDesc: String
     private lateinit var topicsString: String
     private lateinit var updatesString: String
     private lateinit var tryAnotherSearchString: String
@@ -64,6 +66,7 @@ class SearchScreenTest {
     fun setup() {
         composeTestRule.activity.apply {
             clearSearchContentDesc = getString(R.string.clear_search_text_content_desc)
+            clearRecentSearchesContentDesc = getString(R.string.clear_recent_searches_content_desc)
             followButtonContentDesc =
                 getString(interestsR.string.card_follow_button_content_desc)
             unfollowButtonContentDesc =
@@ -82,10 +85,7 @@ class SearchScreenTest {
         }
 
         composeTestRule
-            .onNodeWithContentDescription(clearSearchContentDesc)
-            // The parent of the IconButton whose contentDescription matches the clearSearchText
-            // should be the TextField for search
-            .onParent()
+            .onNodeWithTag("searchTextField")
             .assertIsFocused()
     }
 
@@ -93,7 +93,7 @@ class SearchScreenTest {
     fun emptySearchResult_emptyScreenIsDisplayed() {
         composeTestRule.setContent {
             SearchScreen(
-                uiState = SearchResultUiState.Success(),
+                searchResultUiState = SearchResultUiState.Success(),
             )
         }
 
@@ -106,7 +106,7 @@ class SearchScreenTest {
     fun searchResultWithTopics_allTopicsAreVisible_followButtonsVisibleForTheNumOfFollowedTopics() {
         composeTestRule.setContent {
             SearchScreen(
-                uiState = SearchResultUiState.Success(topics = followableTopicTestData),
+                searchResultUiState = SearchResultUiState.Success(topics = followableTopicTestData),
             )
         }
 
@@ -135,7 +135,7 @@ class SearchScreenTest {
     fun searchResultWithNewsResources_firstNewsResourcesIsVisible() {
         composeTestRule.setContent {
             SearchScreen(
-                uiState = SearchResultUiState.Success(
+                searchResultUiState = SearchResultUiState.Success(
                     newsResources = newsResourcesTestData.map {
                         UserNewsResource(
                             newsResource = it,
@@ -151,6 +151,32 @@ class SearchScreenTest {
             .assertIsDisplayed()
         composeTestRule
             .onNodeWithText(newsResourcesTestData[0].title)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun emptyQuery_notEmptyRecentSearches_verifyClearSearchesButton_displayed() {
+        val recentSearches = listOf("kotlin", "testing")
+        composeTestRule.setContent {
+            SearchScreen(
+                searchResultUiState = SearchResultUiState.EmptyQuery,
+                recentSearchesUiState = RecentSearchQueriesUiState.Success(
+                    recentQueries =
+                    recentSearches.map {
+                        RecentSearchQuery(it)
+                    },
+                ),
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(clearRecentSearchesContentDesc)
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithText("kotlin")
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithText("testing")
             .assertIsDisplayed()
     }
 }

feature/search/src/main/java/com/google/samples/apps/nowinandroid/feature/search/RecentSearchQueriesUiState.kt
@@ -1,0 +1,27 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid.feature.search
+
+import com.google.samples.apps.nowinandroid.core.data.repository.RecentSearchQuery
+
+sealed interface RecentSearchQueriesUiState {
+    object Loading : RecentSearchQueriesUiState
+
+    data class Success(
+        val recentQueries: List<RecentSearchQuery> = emptyList(),
+    ) : RecentSearchQueriesUiState
+}

android/nowinandroid

core/domain/src/main/java/com/google/samples/apps/nowinandroid/core/domain/GetRecentSearchQueriesUseCase.kt
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid.core.domain
+
+import com.google.samples.apps.nowinandroid.core.data.model.RecentSearchQuery
+import com.google.samples.apps.nowinandroid.core.data.repository.RecentSearchRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+/**
+ * A use case which returns the recent search queries.
+ */
+class GetRecentSearchQueriesUseCase @Inject constructor(
+    private val recentSearchRepository: RecentSearchRepository,
+) {
+    operator fun invoke(limit: Int = 10): Flow<List<RecentSearchQuery>> =
+        recentSearchRepository.getRecentSearchQueries(limit)
+}

core/testing/src/main/java/com/google/samples/apps/nowinandroid/core/testing/repository/TestRecentSearchRepository.kt
@@ -1,0 +1,41 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid.core.testing.repository
+
+import com.google.samples.apps.nowinandroid.core.data.model.RecentSearchQuery
+import com.google.samples.apps.nowinandroid.core.data.repository.RecentSearchRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class TestRecentSearchRepository : RecentSearchRepository {
+
+    private val cachedRecentSearches: MutableList<RecentSearchQuery> = mutableListOf()
+
+    override fun getRecentSearchQueries(limit: Int): Flow<List<RecentSearchQuery>> {
+        return flow {
+            emit(cachedRecentSearches.sortedByDescending { it.queriedDate }.take(limit))
+        }
+    }
+
+    override suspend fun insertOrReplaceRecentSearch(searchQuery: String) {
+        cachedRecentSearches.add(RecentSearchQuery(searchQuery))
+    }
+
+    override suspend fun clearRecentSearches() {
+        cachedRecentSearches.clear()
+    }
+}

feature/search/src/androidTest/java/com/google/samples/apps/nowinandroid/feature/search/SearchScreenTest.kt
@@ -23,8 +23,9 @@ import androidx.compose.ui.test.assertIsFocused
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onAllNodesWithContentDescription
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.onParent
+import com.google.samples.apps.nowinandroid.core.data.model.RecentSearchQuery
 import com.google.samples.apps.nowinandroid.core.domain.model.UserNewsResource
 import com.google.samples.apps.nowinandroid.core.model.data.DarkThemeConfig.DARK
 import com.google.samples.apps.nowinandroid.core.model.data.ThemeBrand.ANDROID
@@ -47,6 +48,7 @@ class SearchScreenTest {
     private lateinit var clearSearchContentDesc: String
     private lateinit var followButtonContentDesc: String
     private lateinit var unfollowButtonContentDesc: String
+    private lateinit var clearRecentSearchesContentDesc: String
     private lateinit var topicsString: String
     private lateinit var updatesString: String
     private lateinit var tryAnotherSearchString: String
@@ -64,6 +66,7 @@ class SearchScreenTest {
     fun setup() {
         composeTestRule.activity.apply {
             clearSearchContentDesc = getString(R.string.clear_search_text_content_desc)
+            clearRecentSearchesContentDesc = getString(R.string.clear_recent_searches_content_desc)
             followButtonContentDesc =
                 getString(interestsR.string.card_follow_button_content_desc)
             unfollowButtonContentDesc =
@@ -82,10 +85,7 @@ class SearchScreenTest {
         }
 
         composeTestRule
-            .onNodeWithContentDescription(clearSearchContentDesc)
-            // The parent of the IconButton whose contentDescription matches the clearSearchText
-            // should be the TextField for search
-            .onParent()
+            .onNodeWithTag("searchTextField")
             .assertIsFocused()
     }
 
@@ -93,7 +93,7 @@ class SearchScreenTest {
     fun emptySearchResult_emptyScreenIsDisplayed() {
         composeTestRule.setContent {
             SearchScreen(
-                uiState = SearchResultUiState.Success(),
+                searchResultUiState = SearchResultUiState.Success(),
             )
         }
 
@@ -106,7 +106,7 @@ class SearchScreenTest {
     fun searchResultWithTopics_allTopicsAreVisible_followButtonsVisibleForTheNumOfFollowedTopics() {
         composeTestRule.setContent {
             SearchScreen(
-                uiState = SearchResultUiState.Success(topics = followableTopicTestData),
+                searchResultUiState = SearchResultUiState.Success(topics = followableTopicTestData),
             )
         }
 
@@ -135,7 +135,7 @@ class SearchScreenTest {
     fun searchResultWithNewsResources_firstNewsResourcesIsVisible() {
         composeTestRule.setContent {
             SearchScreen(
-                uiState = SearchResultUiState.Success(
+                searchResultUiState = SearchResultUiState.Success(
                     newsResources = newsResourcesTestData.map {
                         UserNewsResource(
                             newsResource = it,
@@ -151,6 +151,31 @@ class SearchScreenTest {
             .assertIsDisplayed()
         composeTestRule
             .onNodeWithText(newsResourcesTestData[0].title)
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun emptyQuery_notEmptyRecentSearches_verifyClearSearchesButton_displayed() {
+        val recentSearches = listOf("kotlin", "testing")
+        composeTestRule.setContent {
+            SearchScreen(
+                searchResultUiState = SearchResultUiState.EmptyQuery,
+                recentSearchesUiState = RecentSearchQueriesUiState.Success(
+                    recentQueries = recentSearches.map {
+                        RecentSearchQuery(it)
+                    },
+                ),
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(clearRecentSearchesContentDesc)
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithText("kotlin")
+            .assertIsDisplayed()
+        composeTestRule
+            .onNodeWithText("testing")
             .assertIsDisplayed()
     }
 }

feature/search/src/main/java/com/google/samples/apps/nowinandroid/feature/search/RecentSearchQueriesUiState.kt
@@ -1,0 +1,27 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.apps.nowinandroid.feature.search
+
+import com.google.samples.apps.nowinandroid.core.data.model.RecentSearchQuery
+
+sealed interface RecentSearchQueriesUiState {
+    object Loading : RecentSearchQueriesUiState
+
+    data class Success(
+        val recentQueries: List<RecentSearchQuery> = emptyList(),
+    ) : RecentSearchQueriesUiState
+}