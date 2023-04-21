9a7cac05335303729da0aa7cc23c79bfd0ca75d0
Takeshi Hagikura <thagikura@google.com>
2023-04-21 13:16:29 +0900

    Fix the comments in DatabaseModule and SearchResult

--- core/data/src/main/java/com/google/samples/apps/nowinandroid/core/data/model/SearchResult.kt.kt
+++ core/data/src/main/java/com/google/samples/apps/nowinandroid/core/data/model/SearchResult.kt.kt
@@ -19,7 +19,7 @@ package com.google.samples.apps.nowinandroid.core.data.model
 import com.google.samples.apps.nowinandroid.core.model.data.NewsResource
 import com.google.samples.apps.nowinandroid.core.model.data.Topic
 
-/** */
+/** An entity that holds the search result */
 data class SearchResult(
     val topics: List<Topic> = emptyList(),
     val newsResources: List<NewsResource> = emptyList(),
