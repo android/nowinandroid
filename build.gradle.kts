c58637d2662d9c29fc7d35eb5d2e2ce1722d2b92
vishesh211 <visheshtiwari211@gmail.com>
2023-05-12 09:20:37 +0530

    remove application from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,6 @@ buildscript {
 }
 
 plugins {
-    application
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
