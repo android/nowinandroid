d562d566a8eab632cdfb5ee67ed7dd62b3bbc20c
Simon Marquis <contact@simon-marquis.fr>
2023-05-27 14:44:11 +0200

    Android Lint improvements
    - Create `LintConventionPlugin` to configure Lint on all compatible modules: Android applications, Android libraries and JVM modules.
    - Run `lintProdRelease` in CI instead of the default `lintDemoDebug` which is less important compared to production code.
    - Rearrange CI steps to make it more clear that Lint should is an additional step after build (build -> test -> lint).
    - Enable SARIF support and upload results to GitHub's CodeQL to get inline feedback on PRs.
    
    If we really need better perfs, we could restore `lint.checkDependencies = true` on the `:app` module and only execute `:app:lintProdRelease`.
    But in practice, this does not change the total build time on this project.

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -35,6 +35,7 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("com.android.library")
                 apply("org.jetbrains.kotlin.android")
+                apply("nowinandroid.android.lint")
             }
 
             extensions.configure<LibraryExtension> {
