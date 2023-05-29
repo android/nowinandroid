b8e364537f2a50618e4ebc77f96370633300819b
Tomáš Mlynarič <mlykotom@google.com>
2023-05-29 17:33:25 +0200

    Move waitAndFind + dumpWindowHierarchy to Utils
    Change-Id: Id4e05bf2392553179672155b9c1f935a933525dc

--- benchmarks/src/main/java/com/google/samples/apps/nowinandroid/Utils.kt
+++ benchmarks/src/main/java/com/google/samples/apps/nowinandroid/Utils.kt
@@ -16,10 +16,13 @@
 
 package com.google.samples.apps.nowinandroid
 
+import androidx.test.uiautomator.BySelector
 import androidx.test.uiautomator.Direction
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.UiObject2
+import androidx.test.uiautomator.Until
 import com.google.samples.apps.nowinandroid.benchmarks.BuildConfig
+import java.io.ByteArrayOutputStream
 
 /**
  * Convenience parameter to use proper package name with regards to build type and build flavor.
@@ -37,4 +40,25 @@ fun UiDevice.flingElementDownUp(element: UiObject2) {
     element.fling(Direction.DOWN)
     waitForIdle()
     element.fling(Direction.UP)
+}
+
+/**
+ * Waits until an object with [selector] if visible on screen and returns the object.
+ * If the element is not available in [timeout], throws [AssertionError]
+ */
+fun UiDevice.waitAndFindObject(selector: BySelector, timeout: Long): UiObject2 {
+    if (!wait(Until.hasObject(selector), timeout)) {
+        throw AssertionError("Element not found on screen in ${timeout}ms (selector=$selector)")
+    }
+
+    return findObject(selector)
+}
+
+/**
+ * Helper to dump window hierarchy into a string.
+ */
+fun UiDevice.dumpWindowHierarchy(): String {
+    val buffer = ByteArrayOutputStream()
+    dumpWindowHierarchy(buffer)
+    return buffer.toString()
 }
