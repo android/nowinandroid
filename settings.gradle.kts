f8e46b770c12d339af67465b75aa9e488bd57b74
Simon Marquis <contact@simon-marquis.fr>
2023-07-09 10:44:40 +0200

    Kotlin 1.9.0
    - https://github.com/JetBrains/kotlin/releases/tag/v1.9.0
    - https://github.com/google/ksp/releases/tag/1.9.0-1.0.11
    - Compose compiler `1.5.0-dev-k1.9.0-6a60475e07f`

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,6 +28,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://androidx.dev/storage/compose-compiler/repository/")
     }
 }
 rootProject.name = "nowinandroid"
