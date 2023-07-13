d1cc58fa65e3d3b1ef6e14179391135317047d04
Don Turner <donturner@google.com>
2023-07-13 11:34:45 +0100

    Bump version to code=7, name=0.1.1
    Change-Id: I2d8fa940dc5e4c14e3c9df650bc1f1432a330f02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ plugins {
 android {
     defaultConfig {
         applicationId = "com.google.samples.apps.nowinandroid"
-        versionCode = 6
-        versionName = "0.1.0" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+        versionCode = 7
+        versionName = "0.1.1" // X.Y.Z; X = Major, Y = minor, Z = Patch level
 
         // Custom test runner to set up Hilt dependency graph
         testInstrumentationRunner = "com.google.samples.apps.nowinandroid.core.testing.NiaTestRunner"
