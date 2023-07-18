2a00a043e3f9deabc1596cd2b2f2532a6f22f543
Márton Braun <zsmb@google.com>
2023-07-19 01:06:48 +0200

    Update project to Kotlin 1.9
    Change-Id: Ibef85a7a006d67e36fbb642ab0554493731bd49e

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -62,3 +62,10 @@ dependencies {
     testImplementation(project(":core:datastore-test"))
     testImplementation(project(":core:testing"))
 }
+
+androidComponents.beforeVariants {
+    android.sourceSets.register(it.name) {
+        java.srcDir(buildDir.resolve("generated/source/proto/${it.name}/java"))
+        kotlin.srcDir(buildDir.resolve("generated/source/proto/${it.name}/kotlin"))
+    }
+}
