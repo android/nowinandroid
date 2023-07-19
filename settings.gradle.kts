bc0a7e63a13bbaf2c0ace7d25e236ad2906cbc96
Márton Braun <zsmb@google.com>
2023-07-19 09:18:51 +0200

    Update Compose compiler version to 1.5.0
    Change-Id: Ieddb8410a592bb38c5764b7288fd4828306d6743

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -28,7 +28,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven("https://androidx.dev/storage/compose-compiler/repository/")
     }
 }
 rootProject.name = "nowinandroid"
