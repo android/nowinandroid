771c1dff9d965fec45c9951a84e1952f242222c1
Jose Alcérreca <jalc@google.com>
2023-08-18 15:54:57 +0000

    Spotless
    Change-Id: Idb74e4a2c8480b8b4993500267b689aa04303847

--- core/designsystem/src/main/java/com/google/samples/apps/nowinandroid/core/designsystem/component/DynamicAsyncImage.kt
+++ core/designsystem/src/main/java/com/google/samples/apps/nowinandroid/core/designsystem/component/DynamicAsyncImage.kt
@@ -18,7 +18,6 @@ package com.google.samples.apps.nowinandroid.core.designsystem.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
