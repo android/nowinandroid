98619c82415bf79e2616dd89d5c672b51ef8e9ff
전호은 <31261828+jeonhoeun@users.noreply.github.com>
2023-08-31 15:01:55 +0900

    Update core:ui build.gradle.kts
    core:ui is not using core:domain

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -42,7 +42,6 @@ dependencies {
 
     implementation(project(":core:analytics"))
     implementation(project(":core:designsystem"))
-    implementation(project(":core:domain"))
     implementation(project(":core:model"))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.browser)
