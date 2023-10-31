9f268136972ed502f6a443323075ae773834efb5
rosariopf <rosariopf@google.com>
2023-10-31 21:26:53 +0000

    upgrade to Firebase BoM 32.5.0 and drop Firebase KTX

--- core/analytics/src/prod/kotlin/com/google/samples/apps/nowinandroid/core/analytics/AnalyticsModule.kt
+++ core/analytics/src/prod/kotlin/com/google/samples/apps/nowinandroid/core/analytics/AnalyticsModule.kt
@@ -17,8 +17,8 @@
 package com.google.samples.apps.nowinandroid.core.analytics
 
 import com.google.firebase.analytics.FirebaseAnalytics
-import com.google.firebase.analytics.ktx.analytics
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.analytics.analytics
+import com.google.firebase.Firebase
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
