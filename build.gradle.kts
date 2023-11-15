83536d3f0febad7620730f616399ae8e7d6d1f6e
Ben Weiss <benweiss@google.com>
2023-11-15 12:16:57 +0100

    Sort last plugin alphabetically
    Change-Id: I255fbd448d602055c3f395843d7753c7e7d85c86

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ buildscript {
 // Lists all plugins used throughout the project without applying them.
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.test) apply false
     alias(libs.plugins.baselineprofile) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.serialization) apply false
@@ -42,5 +43,4 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.roborazzi) apply false
     alias(libs.plugins.secrets) apply false
-    alias(libs.plugins.android.test) apply false
 }
