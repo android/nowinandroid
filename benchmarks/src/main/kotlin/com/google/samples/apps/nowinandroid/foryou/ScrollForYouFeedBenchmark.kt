ca62bb6c49399288f08889dc9b03c60287438090
Ben Weiss <keyboardsurfer@users.noreply.github.com>
2023-11-15 12:50:15 +0000

    Add automated baseline profile generation (#880)
    Baseline profile generation is disabled for the PR level Build task. Release tasks require a fresh baseline profile. A new profile is generated using the baseline profile Gradle plugin.
    
    * Prepare for usage of dex layout optimizations which can be actively used once NiA switches to AGP 8.2+.
    * Add GMD config to release build
    * Switch to macos-latest
    * Update names for StartupBenchmark tests to better reflect states
    * Stable release and recent GMD device
    * Reduce flakiness by adding wait to benchmark
    * More convenient waiting for objects
    * Rename junit dependency to androidx-junit
    * Only run baseline profile benchmarks during GH workflow
    * Enable automatic BP generation for only release builds
    * Disable BP generation from Build workflow
    * Specify modules and skip benchmarking Build workflow
    
    Bug: b/299334172

--- benchmarks/src/main/kotlin/com/google/samples/apps/nowinandroid/foryou/ScrollForYouFeedBenchmark.kt
+++ benchmarks/src/main/kotlin/com/google/samples/apps/nowinandroid/foryou/ScrollForYouFeedBenchmark.kt
@@ -22,7 +22,7 @@ import androidx.benchmark.macro.StartupMode
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import com.google.samples.apps.nowinandroid.PACKAGE_NAME
-import com.google.samples.apps.nowinandroid.allowNotifications
+import com.google.samples.apps.nowinandroid.startActivityAndAllowNotifications
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -47,8 +47,7 @@ class ScrollForYouFeedBenchmark {
         setupBlock = {
             // Start the app
             pressHome()
-            startActivityAndWait()
-            allowNotifications()
+            startActivityAndAllowNotifications()
         },
     ) {
         forYouWaitForContent()
