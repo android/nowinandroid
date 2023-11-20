e8035622b3baa2aeb792df938a2bbd23e02ee41b
Ben Weiss <benweiss@google.com>
2023-11-20 09:07:30 +0100

    Enable the thread police 👮 for debug builds
    * App will now crash if someone does funky stuff on the main thread.
    
    Change-Id: I9026c100705f2fec6963a1d888b40906186f9d28

--- app/src/main/kotlin/com/google/samples/apps/nowinandroid/NiaApplication.kt
+++ app/src/main/kotlin/com/google/samples/apps/nowinandroid/NiaApplication.kt
@@ -17,6 +17,8 @@
 package com.google.samples.apps.nowinandroid
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.os.StrictMode
 import coil.ImageLoader
 import coil.ImageLoaderFactory
 import com.google.samples.apps.nowinandroid.sync.initializers.Sync
@@ -37,10 +39,25 @@ class NiaApplication : Application(), ImageLoaderFactory {
 
     override fun onCreate() {
         super.onCreate()
+
+        // Kill NiA if there are main thread policy violations and log the offending call.
+        if (isDebuggable()) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().penaltyDeath().build(),
+            )
+        }
+
         // Initialize Sync; the system responsible for keeping data in the app up to date.
         Sync.initialize(context = this)
         profileVerifierLogger()
     }
 
     override fun newImageLoader(): ImageLoader = imageLoader.get()
+
+    /**
+     * Check if the application is debuggable.
+     */
+    private fun isDebuggable(): Boolean {
+        return 0 != applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE
+    }
 }
