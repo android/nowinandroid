5f7d4ea1d57cf497ff830875ae6d2fa0fcc7f7ee
Simon Marquis <contact@simon-marquis.fr>
2023-12-09 11:28:12 +0100

    Replace custom `RoomSchemaArgProvider` in favor of the new Room Gradle plugin
    > Since Room 2.6.0, a Gradle plugin was released to solve various existing issues in Room regarding having inputs and outputs of schemas via Gradle annotation processor options.
    > Changelog: https://developer.android.com/jetpack/androidx/releases/room#2.6.0-alpha02
    
    See https://github.com/gradle/android-cache-fix-gradle-plugin/issues/544 for reference.

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,4 +43,5 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.roborazzi) apply false
     alias(libs.plugins.secrets) apply false
+    alias(libs.plugins.room) apply false
 }
