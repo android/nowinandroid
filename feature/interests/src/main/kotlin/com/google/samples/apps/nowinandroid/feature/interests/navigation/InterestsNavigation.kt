58ecf312ffa3f0fddfbbbf5798d15f2408b2c5c7
Simon Marquis <contact@simon-marquis.fr>
2023-12-21 18:50:50 +0100

    Update feature/interests/src/main/kotlin/com/google/samples/apps/nowinandroid/feature/interests/navigation/InterestsNavigation.kt

--- feature/interests/src/main/kotlin/com/google/samples/apps/nowinandroid/feature/interests/navigation/InterestsNavigation.kt
+++ feature/interests/src/main/kotlin/com/google/samples/apps/nowinandroid/feature/interests/navigation/InterestsNavigation.kt
@@ -26,9 +26,7 @@ import com.google.samples.apps.nowinandroid.feature.interests.InterestsRoute
 private const val INTERESTS_GRAPH_ROUTE_PATTERN = "interests_graph"
 const val INTERESTS_ROUTE = "interests_route"
 
-fun NavController.navigateToInterestsGraph(navOptions: NavOptions) {
-    this.navigate(INTERESTS_GRAPH_ROUTE_PATTERN, navOptions)
-}
+fun NavController.navigateToInterestsGraph(navOptions: NavOptions) = navigate(INTERESTS_GRAPH_ROUTE_PATTERN, navOptions)
 
 fun NavGraphBuilder.interestsGraph(
     onTopicClick: (String) -> Unit,
