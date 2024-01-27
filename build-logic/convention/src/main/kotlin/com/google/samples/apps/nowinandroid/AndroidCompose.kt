db4bf1c78286428892e9aff2701f8b4dd37c1036
Simon Marquis <contact@simon-marquis.fr>
2024-01-27 10:25:24 +0000

    Keep `androidx.compose.ui:ui-tooling-preview` on all buildTypes

--- build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/AndroidCompose.kt
@@ -41,6 +41,7 @@ internal fun Project.configureAndroidCompose(
             val bom = libs.findLibrary("androidx-compose-bom").get()
             add("implementation", platform(bom))
             add("androidTestImplementation", platform(bom))
+            add("implementation", libs.findLibrary("androidx-compose-ui-tooling-preview").get())
             add("debugImplementation", libs.findLibrary("androidx-compose-ui-tooling").get())
         }
 
