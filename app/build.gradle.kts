395b9853df15de34ad76d80e054221583e2a8376
Tomáš Mlynarič <mlykotom@google.com>
2024-01-29 15:59:12 +0100

    Add Composition Tracing
    Change-Id: I3ad60930de2fab71a700a0c2df3f0a1f566cafd8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -104,6 +104,7 @@ dependencies {
     implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.tracing.ktx)
     implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.compose.runtime.tracing)
     implementation(libs.androidx.compose.material3.windowSizeClass)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.profileinstaller)
