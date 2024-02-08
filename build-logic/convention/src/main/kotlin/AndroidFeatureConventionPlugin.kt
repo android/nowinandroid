254b4642592319538d3f290a058b244f6bb7c708
Don Turner <donturner@google.com>
2024-02-08 18:38:51 +0000

    Update Lifecycle to 2.7.0. Use new LifecycleEventEffect.
    Change-Id: I71e7aa521f22499fd3f3e9f3ac7c81658f3abc3a

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -44,6 +44,8 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
                 add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
                 add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
                 add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
+
+                add("androidTestImplementation", libs.findLibrary("androidx.lifecycle.runtimeTesting").get())
             }
         }
     }
