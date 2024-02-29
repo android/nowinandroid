e44fba491732f310ca329653997c64fea217030f
Jonathan Koren <jdkoren@google.com>
2024-02-29 10:00:33 -0800

    Set semantics for selected state
    Change-Id: I31f27d5036d07c9607909c09ac52a72391f899ca

--- feature/interests/src/main/kotlin/com/google/samples/apps/nowinandroid/feature/interests/InterestsItem.kt
+++ feature/interests/src/main/kotlin/com/google/samples/apps/nowinandroid/feature/interests/InterestsItem.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.selected
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -91,7 +92,9 @@ fun InterestsItem(
             },
         ),
         modifier = modifier
-            .semantics(mergeDescendants = true) { /* no-op */ }
+            .semantics(mergeDescendants = true) {
+                selected = isSelected
+            }
             .clickable(enabled = true, onClick = onClick),
     )
 }
