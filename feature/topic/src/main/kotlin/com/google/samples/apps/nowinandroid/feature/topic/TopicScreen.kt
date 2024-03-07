e6d9394de9b2afcdb07637f3d12f8f5c954f6d69
Jonathan Koren <jdkoren@google.com>
2024-03-07 11:08:03 -0800

    Update multipleBackStackInterests test
    Change-Id: I1e372f7989817151a6765205291b13b561187fa8

--- feature/topic/src/main/kotlin/com/google/samples/apps/nowinandroid/feature/topic/TopicScreen.kt
+++ feature/topic/src/main/kotlin/com/google/samples/apps/nowinandroid/feature/topic/TopicScreen.kt
@@ -45,6 +45,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -84,7 +85,7 @@ internal fun TopicRoute(
     TopicScreen(
         topicUiState = topicUiState,
         newsUiState = newsUiState,
-        modifier = modifier,
+        modifier = modifier.testTag("topic:${viewModel.topicId}"),
         showBackButton = showBackButton,
         onBackClick = onBackClick,
         onFollowClick = viewModel::followTopicToggle,
