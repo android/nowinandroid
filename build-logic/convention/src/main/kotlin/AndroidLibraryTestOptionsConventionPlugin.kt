1b529e6859780f28fccf6dcc6cd41f2299b1371e
Jaehwa Noh <shwoghk14@gmail.com>
2024-05-16 19:02:51 +0900

    Create AndroidLibraryTestOptionsConventionPlugin.
    Change-Id: Iaa4421b13b37ce463e61261e976c0a583207bb18

---
+++ build-logic/convention/src/main/kotlin/AndroidLibraryTestOptionsConventionPlugin.kt
@@ -0,0 +1,33 @@
+
+import com.android.build.api.dsl.LibraryExtension
+import com.google.samples.apps.nowinandroid.configureTestOptions
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.getByType
+
+/*
+ * Copyright 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class AndroidLibraryTestOptionsConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("com.android.library")
+
+            val extension = extensions.getByType<LibraryExtension>()
+            configureTestOptions(extension)
+        }
+    }
+}
