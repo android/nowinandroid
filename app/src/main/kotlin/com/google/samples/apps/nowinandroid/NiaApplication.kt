039ca889cf112a5d4ca294b99a0db47962e8467d
Jaehwa Noh <shwoghk14@gmail.com>
2024-06-06 16:52:50 +0900

    Replace app startup library.
    Change-Id: I26b6002ccdf80870ecac28956166eac93b1f83ed

--- app/src/main/kotlin/com/google/samples/apps/nowinandroid/NiaApplication.kt
+++ app/src/main/kotlin/com/google/samples/apps/nowinandroid/NiaApplication.kt
@@ -20,7 +20,6 @@ import android.app.Application
 import coil.ImageLoader
 import coil.ImageLoaderFactory
 import com.google.samples.apps.nowinandroid.sync.initializers.Sync
-import com.google.samples.apps.nowinandroid.util.ProfileVerifierLogger
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
@@ -32,14 +31,10 @@ class NiaApplication : Application(), ImageLoaderFactory {
     @Inject
     lateinit var imageLoader: dagger.Lazy<ImageLoader>
 
-    @Inject
-    lateinit var profileVerifierLogger: ProfileVerifierLogger
-
     override fun onCreate() {
         super.onCreate()
         // Initialize Sync; the system responsible for keeping data in the app up to date.
         Sync.initialize(context = this)
-        profileVerifierLogger()
     }
 
     override fun newImageLoader(): ImageLoader = imageLoader.get()
