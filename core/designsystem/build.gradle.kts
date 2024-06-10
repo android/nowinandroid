616b346f79f19f0e1c4164f45bc3588faf37e25f
lihenggui <solo8web@gmail.com>
2024-06-10 11:39:24 -0700

    Upgrade dependencies

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -69,23 +69,4 @@ kotlin {
 
 dependencies {
     lintPublish(projects.lint)
-
-    api(libs.androidx.compose.foundation)
-    api(libs.androidx.compose.foundation.layout)
-    api(libs.androidx.compose.material.iconsExtended)
-    api(libs.androidx.compose.material3)
-    api(libs.androidx.compose.runtime)
-    api(libs.androidx.compose.ui.util)
-
-    implementation(libs.coil.kt.compose)
-
-    testImplementation(libs.androidx.compose.ui.test)
-    testImplementation(libs.hilt.android.testing)
-    testImplementation(libs.robolectric)
-    testImplementation(libs.roborazzi)
-    testImplementation(projects.core.screenshotTesting)
-    testImplementation(projects.core.testing)
-
-    androidTestImplementation(libs.androidx.compose.ui.test)
-    androidTestImplementation(projects.core.testing)
 }
