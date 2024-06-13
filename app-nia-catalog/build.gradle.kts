80adc9a719074028d53623b69ff27224be1fb5d2
lihenggui <solo8web@gmail.com>
2024-06-12 17:18:54 -0700

    Upgrade dependencies

--- app-nia-catalog/build.gradle.kts
+++ app-nia-catalog/build.gradle.kts
@@ -90,9 +90,9 @@ android {
         applicationId = "com.google.samples.apps.niacatalog"
         versionCode = 1
         versionName = "0.0.1" // X.Y.Z; X = Major, Y = minor, Z = Patch level
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = 24
+        targetSdk = 34
+        compileSdk = 34
         // The UI catalog does not depend on content from the app, however, it depends on modules
         // which do, so we must specify a default value for the contentType dimension.
         missingDimensionStrategy("contentType", "demo")
