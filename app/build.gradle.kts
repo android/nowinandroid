5d861df0469f19e28c514135153c0ba5ecce3308
Ben Weiss <benweiss@google.com>
2024-09-05 17:11:17 +0200

    Enable Startup Profile generation for release builds
    Change-Id: I7eb5d911eb1d22c85d4c89ccfcdac913bea9943a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -137,6 +137,9 @@ baselineProfile {
     // Don't build on every iteration of a full assemble.
     // Instead enable generation directly for the release build variant.
     automaticGenerationDuringBuild = false
+
+    // Make use of Dex Layout Optimizations via Startup Profiles
+    dexLayoutOptimization = true
 }
 
 dependencyGuard {
