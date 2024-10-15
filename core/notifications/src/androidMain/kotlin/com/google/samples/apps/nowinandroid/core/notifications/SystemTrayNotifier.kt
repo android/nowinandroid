454acf5d3db75f620d364b03dd8adfe3c423ee91
lihenggui <solo8web@gmail.com>
2024-10-15 11:41:55 -0700

    Move public constants in the common module

--- core/notifications/src/androidMain/kotlin/com/google/samples/apps/nowinandroid/core/notifications/SystemTrayNotifier.kt
+++ core/notifications/src/androidMain/kotlin/com/google/samples/apps/nowinandroid/core/notifications/SystemTrayNotifier.kt
@@ -40,11 +40,6 @@ private const val NEWS_NOTIFICATION_REQUEST_CODE = 0
 private const val NEWS_NOTIFICATION_SUMMARY_ID = 1
 private const val NEWS_NOTIFICATION_CHANNEL_ID = ""
 private const val NEWS_NOTIFICATION_GROUP = "NEWS_NOTIFICATIONS"
-private const val DEEP_LINK_SCHEME_AND_HOST = "https://www.nowinandroid.apps.samples.google.com"
-private const val DEEP_LINK_FOR_YOU_PATH = "foryou"
-private const val DEEP_LINK_BASE_PATH = "$DEEP_LINK_SCHEME_AND_HOST/$DEEP_LINK_FOR_YOU_PATH"
-const val DEEP_LINK_NEWS_RESOURCE_ID_KEY = "linkedNewsResourceId"
-const val DEEP_LINK_URI_PATTERN = "$DEEP_LINK_BASE_PATH/{$DEEP_LINK_NEWS_RESOURCE_ID_KEY}"
 
 /**
  * Implementation of [Notifier] that displays notifications in the system tray.
