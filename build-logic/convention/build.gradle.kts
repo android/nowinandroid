2b33683ecbb791890a2b49e042f9ba6c2d9dc568
lihenggui <solo8web@gmail.com>
2024-11-15 14:52:14 -0800

    Remove plugin definition

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -58,18 +58,6 @@ gradlePlugin {
             id = "nowinandroid.android.application.jacoco"
             implementationClass = "AndroidApplicationJacocoConventionPlugin"
         }
-        register("androidLibraryCompose") {
-            id = "nowinandroid.android.library.compose"
-            implementationClass = "AndroidLibraryComposeConventionPlugin"
-        }
-        register("androidLibrary") {
-            id = "nowinandroid.android.library"
-            implementationClass = "AndroidLibraryConventionPlugin"
-        }
-        register("androidFeature") {
-            id = "nowinandroid.android.feature"
-            implementationClass = "AndroidFeatureConventionPlugin"
-        }
         register("androidLibraryJacoco") {
             id = "nowinandroid.android.library.jacoco"
             implementationClass = "AndroidLibraryJacocoConventionPlugin"
