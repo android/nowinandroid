72bcf493f3d9bc67c124cef39feed1c079b12819
Yuri Schimke <yuri@schimke.ee>
2024-11-18 10:13:08 +0000

    remove sdk 21

--- core/designsystem/src/test/kotlin/com/google/samples/apps/nowinandroid/core/designsystem/TopAppBarScreenshotTests.kt
+++ core/designsystem/src/test/kotlin/com/google/samples/apps/nowinandroid/core/designsystem/TopAppBarScreenshotTests.kt
@@ -43,7 +43,7 @@ import org.robolectric.annotation.LooperMode
 @OptIn(ExperimentalMaterial3Api::class)
 @RunWith(RobolectricTestRunner::class)
 @GraphicsMode(GraphicsMode.Mode.NATIVE)
-@Config(application = HiltTestApplication::class, qualifiers = "480dpi", sdk = [21])
+@Config(application = HiltTestApplication::class, qualifiers = "480dpi")
 @LooperMode(LooperMode.Mode.PAUSED)
 class TopAppBarScreenshotTests {
 
