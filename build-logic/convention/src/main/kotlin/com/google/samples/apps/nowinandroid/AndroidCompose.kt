2a180f9d49075ab87fdf0fa44d3e7f94882a476b
Simon Marquis <contact@simon-marquis.fr>
2024-12-19 10:26:22 +0100

    Use lambda syntax

--- build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/AndroidCompose.kt
@@ -67,7 +67,8 @@ internal fun Project.configureAndroidCompose(
             .relativeToRootProject("compose-reports")
             .let(reportsDestination::set)
 
-        stabilityConfigurationFiles
-            .add(isolated.rootProject.projectDirectory.file("compose_compiler_config.conf"))
+        stabilityConfigurationFiles.add {
+            isolated.rootProject.projectDirectory.file("compose_compiler_config.conf")
+        }
     }
 }
