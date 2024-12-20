9320d592b40b793159c8b62e69dca5e25a351298
Jaehwa Noh <shwoghk14@gmail.com>
2024-12-20 21:18:42 +0900

    Override readText.
    Change-Id: Ibf7fca9de5461e7285ed6d68092e36d70408f3bd

--- core/network/src/main/kotlin/JvmUnitTestDemoAssetManager.kt
+++ core/network/src/main/kotlin/JvmUnitTestDemoAssetManager.kt
@@ -17,6 +17,7 @@
 import com.google.samples.apps.nowinandroid.core.network.demo.DemoAssetManager
 import java.io.File
 import java.io.InputStream
+import java.nio.charset.StandardCharsets
 import java.util.Properties
 
 /**
@@ -38,4 +39,5 @@ internal object JvmUnitTestDemoAssetManager : DemoAssetManager {
     private val assets = File(properties["android_merged_assets"].toString())
 
     override fun open(fileName: String): InputStream = File(assets, fileName).inputStream()
+    override fun readText(fileName: String): String = File(assets, fileName).readText(StandardCharsets.UTF_8)
 }
