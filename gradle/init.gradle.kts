c6312a7fdb12bc512695079f2d6d901ddd1262b5
OTVENTURES\Amal.Sakr <amal.sakr@linkdev.com>
2025-03-15 07:11:36 +0200

    Refine Spotless configuration to avoid wildcards

--- gradle/init.gradle.kts
+++ gradle/init.gradle.kts
@@ -33,8 +33,8 @@ rootProject {
         apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
         extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
             kotlin {
-                target("**/*.kt")
-                targetExclude("**/build/**/*.kt")
+                target("src/**/*.kt")
+                targetExclude("**/build/**")
                 ktlint(ktlintVersion).editorConfigOverride(
                     mapOf(
                         "android" to "true",
@@ -44,13 +44,13 @@ rootProject {
             }
             format("kts") {
                 target("**/*.kts")
-                targetExclude("**/build/**/*.kts")
+                targetExclude("**/build/**")
                 // Look for the first line that doesn't have a block comment (assumed to be the license)
                 licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
             }
             format("xml") {
-                target("**/*.xml")
-                targetExclude("**/build/**/*.xml")
+                target("src/**/*.xml")
+                targetExclude("**/build/**")
                 // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
                 licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
             }
