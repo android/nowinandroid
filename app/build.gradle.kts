304bea15d0a1af5f5be218069d4a18b943a05ffc
Clara Fok <clarafok@google.com>
2025-07-11 14:30:27 -0700

    Add NiaBackStack and BackStack providers
    Bump agp versions and add navigation 3 dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,7 @@ dependencies {
     implementation(projects.core.designsystem)
     implementation(projects.core.data)
     implementation(projects.core.model)
+    implementation(projects.core.navigation)
     implementation(projects.core.analytics)
     implementation(projects.sync.work)
 
--- build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
@@ -35,7 +35,7 @@ internal fun Project.configureKotlinAndroid(
     commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
     commonExtension.apply {
-        compileSdk = 35
+        compileSdk = 36
 
         defaultConfig {
             minSdk = 23
