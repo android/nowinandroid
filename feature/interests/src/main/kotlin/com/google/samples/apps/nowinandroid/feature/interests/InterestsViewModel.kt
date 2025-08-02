a775e750ecc1d0c60c625366888b96ce25e5059c
명훈 <s23065@gsm.hs.kr>
2025-08-03 03:40:07 +0900

    Refactor InterestsViewModel ViewModels to internal modifier

--- feature/interests/src/main/kotlin/com/google/samples/apps/nowinandroid/feature/interests/InterestsViewModel.kt
+++ feature/interests/src/main/kotlin/com/google/samples/apps/nowinandroid/feature/interests/InterestsViewModel.kt
@@ -34,7 +34,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class InterestsViewModel @Inject constructor(
+internal class InterestsViewModel @Inject constructor(
     private val savedStateHandle: SavedStateHandle,
     val userDataRepository: UserDataRepository,
     getFollowableTopics: GetFollowableTopicsUseCase,
