374ed8b002ffec37cef2bbf98dbe66b7ca3889ea
Simon Marquis <contact@simon-marquis.fr>
2026-01-09 12:03:04 +0100

    fix: restore removal during merge

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -19,6 +19,7 @@ import com.android.build.api.variant.ApplicationAndroidComponentsExtension
 import com.google.samples.apps.nowinandroid.configureBadgingTasks
 import com.google.samples.apps.nowinandroid.configureGradleManagedDevices
 import com.google.samples.apps.nowinandroid.configureKotlinAndroid
+import com.google.samples.apps.nowinandroid.configurePrintApksTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
@@ -38,6 +39,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                 configureGradleManagedDevices(this)
             }
             extensions.configure<ApplicationAndroidComponentsExtension> {
+                configurePrintApksTask(this)
                 configureBadgingTasks(this)
             }
         }
