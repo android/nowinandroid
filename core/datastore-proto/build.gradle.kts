69b7238b10078292c54eda8547d1e4dc44e2a1b5
TM <tjmcard@gmail.com>
2026-01-23 12:37:23 -0800

    Update build.gradle.kts

--- core/datastore-proto/build.gradle.kts
+++ core/datastore-proto/build.gradle.kts
@@ -42,14 +42,6 @@ protobuf {
     }
 }
 
-androidComponents.beforeVariants {
-    android.sourceSets.register(it.name) {
-        val buildDir = layout.buildDirectory.get().asFile
-        java.srcDir(buildDir.resolve("generated/source/proto/${it.name}/java"))
-        kotlin.srcDir(buildDir.resolve("generated/source/proto/${it.name}/kotlin"))
-    }
-}
-
 dependencies {
     api(libs.protobuf.kotlin.lite)
 }
