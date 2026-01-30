3ddc18c02384736bf50d1b234d216aa46987d0b8
Aurimas Liutikas <aurimas@google.com>
2026-01-30 08:24:15 -0800

    Enable spotless for build-logic via root project

--- build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
@@ -103,7 +103,7 @@ private inline fun <reified T : KotlinBaseExtension> Project.configureKotlin() =
              * The binary signature changes. The error on the declaration is no longer reported.
              * '-Xconsistent-data-class-copy-visibility' compiler flag and ConsistentCopyVisibility annotation are now unnecessary.
              */
-            "-Xconsistent-data-class-copy-visibility"
+            "-Xconsistent-data-class-copy-visibility",
         )
     }
 }
--- build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/Spotless.kt
+++ build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/Spotless.kt
@@ -37,13 +37,35 @@ internal fun Project.configureSpotlessForJvm() {
     configureSpotlessCommon()
 }
 
+internal fun Project.configureSpotlessForRootProject() {
+    apply(plugin = "com.diffplug.spotless")
+    extensions.configure<SpotlessExtension> {
+        kotlin {
+            target("build-logic/convention/src/**/*.kt")
+            ktlint(libs.findVersion("ktlint").get().requiredVersion).editorConfigOverride(
+                mapOf("android" to "true"),
+            )
+            licenseHeaderFile(rootDir.resolve("spotless/copyright.kt"))
+            endWithNewline()
+        }
+        format("kts") {
+            target("*.kts")
+            target("build-logic/*.kts")
+            target("build-logic/convention/*.kts")
+            // Look for the first line that doesn't have a block comment (assumed to be the license)
+            licenseHeaderFile(rootDir.resolve("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+            endWithNewline()
+        }
+    }
+}
+
 private fun Project.configureSpotlessCommon() {
     apply(plugin = "com.diffplug.spotless")
     extensions.configure<SpotlessExtension> {
         kotlin {
             target("src/**/*.kt")
             ktlint(libs.findVersion("ktlint").get().requiredVersion).editorConfigOverride(
-                mapOf("android" to "true")
+                mapOf("android" to "true"),
             )
             licenseHeaderFile(rootDir.resolve("spotless/copyright.kt"))
             endWithNewline()
