357ec5ae02b69bd97b566ba1ed60a5f70b3c8b75
Don Turner <donturner@google.com>
2026-03-23 16:11:58 +0000

    Add phone in landscape

--- core/ui/src/main/kotlin/com/google/samples/apps/nowinandroid/core/ui/DevicePreviews.kt
+++ core/ui/src/main/kotlin/com/google/samples/apps/nowinandroid/core/ui/DevicePreviews.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.tooling.preview.Preview
  * to render various devices.
  */
 @Preview(name = "phone", device = Devices.PHONE, showBackground = true)
+@Preview(name = "phone_in_landscape", widthDp = 891, heightDp = 411, showBackground = true)
 @Preview(name = "foldable", device = Devices.FOLDABLE, showBackground = true)
 @Preview(name = "tablet", device = Devices.TABLET, showBackground = true)
 @Preview(name = "desktop", device = Devices.DESKTOP, showBackground = true)
